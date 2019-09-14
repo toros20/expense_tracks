b4c5aef5a6fd6bf8beb454915286a682448677f5
Marvin Toro <marvinlink07@gmail.com>
2019-09-13 23:57:14 -0600

    create firs route for accounts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import express, { json} from 'express';
 import morgan from 'morgan';
+
+//initializacion
 const app = express();
+
+//importing routes
+import accountsRoutes from './routes/acounts';
+
+
+//routes
+app.use(accountsRoutes);
+
 
 //midellwares
 app.use(morgan('dev'));
