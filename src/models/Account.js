7dd72a35a7b1b18f4c0a10f819da5062c5ee05c0
Marvin Toro <marvinlink07@gmail.com>
2019-09-14 10:00:47 -0600

    define Account object

--- src/models/Account.js
+++ src/models/Account.js
@@ -4,12 +4,27 @@ import Sequelize from 'sequelize';
 //import connection object
 import { sequelize } from '../database/database';
 
-sequelize.define('accounts',{
+const Account = sequelize.define('accounts',{
     id:{
         type: Sequelize.INTEGER,
         primaryKey:true
     },
-    
-}
-   
-);
+    name:{
+        type: Sequelize.TEXT,
+    },
+    detail:{
+        type:Sequelize.TEXT
+    },
+    balance:{
+        type:Sequelize.FLOAT
+    },
+    type:{
+        type:Sequelize.TEXT
+    },
+    user_id:{
+        type:sequelize.INTEGER
+    }
+
+},{timestamps:false });
+
+export default Account;
