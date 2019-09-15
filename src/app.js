2a5ea6d1b1cce3d39f39558c095f5e4fe8ce4191
Marvin Toro <marvinlink07@gmail.com>
2019-09-14 18:36:18 -0600

    update packet json

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 import express, { json } from 'express';
 import morgan from 'morgan';
 
-//initializacion
-const app = express();
-
 //importing routes
 import accountsRoutes from './routes/acounts';
 import categoriesRoutes from './routes/categories';
+
+//initializacion
+const app = express();
+
+//midellwares
+app.use(morgan('dev'));
+app.use(json());
 
 //routes
 app.use('/api/accounts',accountsRoutes);
 app.use('/api/categories',categoriesRoutes);
 
-
-//midellwares
-app.use(morgan('dev'));
-app.use(json());
 
 export default app;
