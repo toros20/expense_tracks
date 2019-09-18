5c45073cb77063cdf90122712c86be03c63c5683
Marvin Toro <marvinlink07@gmail.com>
2019-09-17 23:03:09 -0600

    testing singup form by post request

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,10 @@
-import { render } from "timeago.js";
+
 
 export async function signup(req,res){
     res.render('auth/signup');
+}
+
+export async function signupAuth(req,res){
+    res.send('recibido');
+    console.log(req.body);
 }
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,10 +1,13 @@
 import {Router} from 'express';
 const router = Router();
 
-import {signup} from '../controllers/authController';
+import {signup,signupAuth} from '../controllers/authController';
 
 //route to show the form to register /api/auth/signup 
 router.get('/signup',signup);
+
+//route to sigun the register /api/auth/signup 
+router.post('/signup',signupAuth);
 
 
 export default router;
