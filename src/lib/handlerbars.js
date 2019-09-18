1110dc0ec866446cfe6fbeffa59fa84e5e051f45
Marvin Toro <marvinlink07@gmail.com>
2019-09-17 20:34:14 -0600

    create new helper ifvalue

--- src/lib/handlerbars.js
+++ src/lib/handlerbars.js
@@ -1,10 +1,18 @@
 import {format} from "timeago.js";
 
-const helpers = {};
+const helpers = {
+    ifvalue: function(conditional, options) {
+        if (conditional == options.hash.equals) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+    }
+};
 
 helpers.timeago = (timestamp) => {
     return format(timestamp);
 };
-
+ 
 module.exports = helpers;  
-
+ 
