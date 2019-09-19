aae133f7a1f64a65e1e134ab84bf8387efae109c
Marvin Toro <marvinlink07@gmail.com>
2019-09-18 23:06:49 -0600

    create the income MODEL

---
+++ src/models/Income.js
@@ -0,0 +1,41 @@
+
+import Sequelize from 'sequelize';
+
+//import connection objet
+import { sequelize } from '../database/database';
+
+const Income = sequelize.define('incomes',{
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        aotuIncrement: true,
+    },
+    name:{
+        type: Sequelize.STRING
+    },
+    details:{
+        type:Sequelize.TEXT
+    },
+    quantity:{
+        type:Sequelize.FLOAT
+    },
+    account_id:{
+        type: Sequelize.INTEGER
+    },
+    user_id:{
+        type:Sequelize.INTEGER
+    },
+    createdAt:{
+        type:Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        
+    },
+    updatedAt:{
+        type:Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        
+    }
+
+},{timestamps: true});
+
+export default Income;
