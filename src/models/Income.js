7125c9a58883c6896ea5373531b1917a44678365
Marvin Toro <marvinlink07@gmail.com>
2019-09-19 20:00:52 -0600

    create the expense module

--- src/models/Income.js
+++ src/models/Income.js
@@ -1,4 +1,3 @@
-
 import Sequelize from 'sequelize';
 
 //import connection objet
@@ -8,7 +7,7 @@ const Income = sequelize.define('incomes',{
     id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        aotuIncrement: true,
+        autoIncrement: true,
     },
     name:{
         type: Sequelize.STRING
---
+++ src/routes/expenses.js
@@ -0,0 +1,28 @@
+import {Router} from 'expres';
+const router = Router();
+
+import {listExpenses,createExpense,deleteExpense,updateExpense} 
+from  '../controllers/expensesController';
+
+//module to verificated users are login
+const {isLoggedIn} = require('../lib/auth');
+
+//route to show all the expenses
+router.get('/list',isLoggedIn,listExpenses);
+
+//route to show the form to create a new Expense /api/expenses/create
+router.get('/create',isLoggedIn,addExpense);
+
+//route to save the new expense /api/expenses/
+router.post('/',isLoggedIn,createExpense);
+
+//route  to show one especific expense
+router.get('./:id',isLoggedIn,showExpense);
+
+//route to delete an especific expense
+router.delete('./:id',isLoggedIn,deleteExpense);
+
+//route to update an especific expense
+router.put('/:id',isLoggedIn,updateExpense);
+
+export default router;
